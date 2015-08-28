d58fe89aa1a2c5f20794a23e8a77351a9537afd1
Julian Haeger <julianhaeger@gmail.com>
2015-08-28 11:31:07 +0100

    reads config from a json file in a configurable location

--- lib/meagle/service_instance_config.ex
+++ lib/meagle/service_instance_config.ex
@@ -1,8 +1,22 @@
 defmodule Meagle.Config do
 	def all do
-		%{"query" => [
-			"http://rates-query-int.laterooms.com/status",
-			"http://rates-query-qa.laterooms.com/status"
-		]}
+		from_file
 	end
+
+	def from_file do
+		config_dir = get_env("CONFIG_DIR", ".")
+
+		config_path = Path.join([config_dir, "monitoringTargets.json"])
+		
+		config_path
+			|> File.read!
+			|> Poison.Parser.parse!
+ 	end
+
+ 	defp get_env(name, default \\ nil) do
+ 		System.get_env 
+			|> Enum.map(fn {k,v} -> {String.upcase(k),v} end)
+			|> Enum.into(%{})
+			|> Dict.get(name, default)
+ 	end
 end
